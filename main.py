477da2b8839f5cbf16838a7b5ad762166f9ed0b4
flo <florian.holeczek@posteo.at>
2025-01-19 13:12:15 +0100

    cleaning

--- main.py
+++ main.py
@@ -17,28 +17,23 @@ np.random.seed(seed)
 device="cpu"
 n_inference=4000
 batch_size=1024
-n_iterations=4000
+n_iterations=1000
 trajectory_length = 3
 
 
 
 env = Env(mus=[torch.tensor([1,1]), torch.tensor([-1.5,-1.5])], sigmas = [torch.ones(2)*0.2]*2,)
 gflownet = GFlowNet(device=device)
-losses, logzs, true_logz = gflownet.train(
+losses_, _ = gflownet.train(
     env,
     n_iterations=n_iterations,
     batch_size=batch_size,
     trajectory_length=trajectory_length,
     off_policy=2.5
 )
+losses, logzs, true_logz = losses_
 fig = plot.plot_losses(losses, logzs, true_logz)
 plt.show()
 trajectories = gflownet.inference(env, batch_size=n_inference, trajectory_length=trajectory_length)
 fig = plot.plot_states_2d(env, trajectories, alpha=0.8, ground_truth="contour", colormap="viridis", levels=10)
 plt.show()
-
-# TODO model saving and loading
-# TODO discretize 1.5 steps (4x4)
-# TODO playground
-# TODO Texts
-# TODO multiprocessing?
